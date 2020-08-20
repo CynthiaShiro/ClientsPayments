0f08e675263d5cb94b7b8e62177da8649e8d522a
CynthiaShiro <aimydewlux@gmail.com>
2020-08-20 09:50:20 +0300

    fourth commit

---
+++ lambda.py
@@ -0,0 +1,23 @@
+#multiply the exclusive amount by the vat
+def myfunc(n):
+    return lambda a : a*n
+#gets the sum of all clients payments
+def getSum(Payment):
+    Payment = lambda a, b, c : a + b + c
+    return Payment
+
+#front end
+thisdict = {
+    "client1" : 1000000,
+    "client2" : 500000,
+    "client3" : 2000
+}
+
+mydoubler = myfunc(0.16)
+new1 = mydoubler(thisdict.get("client1")) + thisdict.get("client1")
+new2 = mydoubler(thisdict.get("client2")) + thisdict.get("client2")
+new3 = mydoubler(thisdict.get("client3")) + thisdict.get("client3")
+x = 0
+mySum = getSum(x)
+Total = mySum(new1, new2, new3)
+print("The Total Inclusive amount is KES {0:,.2f}".format(Total))
